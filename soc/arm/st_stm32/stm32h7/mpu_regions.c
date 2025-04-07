bd575ec261bec81372ab2b4bcd93a4d35c23b88f
Tristan Seifert <tristanseifert@users.noreply.github.com>
2025-04-07 13:55:56 -0700

    stm32h7: map system flash gap area as SO+XN to prevent speculation (#16)

--- soc/arm/st_stm32/stm32h7/mpu_regions.c
+++ soc/arm/st_stm32/stm32h7/mpu_regions.c
@@ -13,6 +13,18 @@ static const struct arm_mpu_region mpu_regions[] = {
 					 REGION_FLASH_ATTR(REGION_FLASH_SIZE)),
 	MPU_REGION_ENTRY("SRAM", CONFIG_SRAM_BASE_ADDRESS,
 					 REGION_RAM_ATTR(REGION_SRAM_SIZE)),
+
+	/*
+	 * Map system flash area as execute never, strongly ordered, to prevent CPU core
+	 * from making speculative accesses.
+	 *
+	 * This results in spurious RDSERR and RDPERR errors from getting asserted.
+	 */
+	MPU_REGION_ENTRY("SYSTEM", 0x1FF00000,
+					{ (STRONGLY_ORDERED_SHAREABLE |
+					REGION_512K |
+					MPU_RASR_XN_Msk | P_RW_U_NA_Msk) }),
+
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(sram3), okay) && \
 		DT_NODE_HAS_STATUS(DT_NODELABEL(mac), okay)
 	MPU_REGION_ENTRY("SRAM3_ETH_BUF",
