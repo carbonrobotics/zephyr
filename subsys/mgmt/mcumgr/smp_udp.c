ecc5923779f27c3304e04a93b9df123d7298c8a6
danielm-cr <danielm@carbonrobotics.com>
2025-06-09 18:41:51 -0700

    Zephyr and mcumgr stack overflow check and buffer overrun check.
    bug fixes from Zephyr mcumgr
    
    Add a compile time assertion, check a buffer size used in mcumgr. 
    
    Add a runtime check for minimum thread stack size. k_panic() is called if Zephyr tries to create a thread with a stack size that is smaller than a known minimum size.

--- subsys/mgmt/mcumgr/smp_udp.c
+++ subsys/mgmt/mcumgr/smp_udp.c
@@ -60,6 +60,9 @@ static struct configs configs = {
 #endif
 };
 
+BUILD_ASSERT(sizeof(struct sockaddr) <= CONFIG_MCUMGR_BUF_USER_DATA_SIZE,
+	     "CONFIG_MCUMGR_BUF_USER_DATA_SIZE must be >= sizeof(struct sockaddr)");
+
 #if CONFIG_MCUMGR_SMP_UDP_IPV4
 static int smp_udp4_tx(struct zephyr_smp_transport *zst, struct net_buf *nb)
 {
