4cb2db9f965a8864a0cf8eb79597e1eb32f134a1
Daniel Mamaghani <danielm@carbonrobotics.com>
2025-06-19 20:19:43 -0700

    Change the zephyr Logging thread priority from the very last priorty (i.e. 15) to -2. At -2 it's higher than the other zephyr threads which are at -1. i.e. sysworkq, conn_mgr, rx_q[0], and net_mgmt. The zephyr stm_eth thread has priority of -14, so still much higher than the logging one.

--- subsys/logging/log_core.c
+++ subsys/logging/log_core.c
@@ -669,7 +669,7 @@ static int enable_logger(const struct device *arg)
 		k_thread_create(&logging_thread, logging_stack,
 				K_KERNEL_STACK_SIZEOF(logging_stack),
 				log_process_thread_func, NULL, NULL, NULL,
-				K_LOWEST_APPLICATION_THREAD_PRIO, 0,
+				-2, 0,
 				COND_CODE_1(CONFIG_LOG_PROCESS_THREAD,
 					K_MSEC(CONFIG_LOG_PROCESS_THREAD_STARTUP_DELAY_MS),
 					K_NO_WAIT));
